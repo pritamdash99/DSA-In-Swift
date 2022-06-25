3d73ac17349ad6abfd91cd0dda8e51145832d7c7
Pritam Dash <pritamdash2015@outlook.com>
2022-06-25 23:36:01 +0530

    3rd Approach added.

--- ArrayProblems/MaxSubArray.playground/Contents.swift
+++ ArrayProblems/MaxSubArray.playground/Contents.swift
@@ -42,4 +42,27 @@ Constraints:
  Approach :
 
  We can also do this problem using only two for loop, starting the function with ( max_sum ) variable which will have the final answer. We can get the sum of all possible subarrays in this way and then return the maximum so far.
+ Solution : 3 Optimal Solution: Kadane’s Algorithm
+
+ Intuition: Basically this problem can be done in linear time complexity with Kadane’s algorithm along with that will also get the subarray which is giving the largest positive-sum.
+
+ Approach:
+
+ -> We will have the following variables in the beginning :
+
+ msf – max so far, meh – max end here, start – to get the starting index of ans’s subarray, end – to get the ending index of ans’s subarray.
+
+ -> Traverse the array starting with adding the ith element with max_end_here(meh) , now we will check that adding the element gives greater value than max_so_far(msf) , if yes then we will update our msf and also update the starting and ending index .
+
+ for(int i=0;i<nums.size();i++){
+     meh+=nums[i];
+
+     if(meh>msf){ msf=meh; start=s; end=i; }
+
+     if(meh<0){meh=0; s=i+1;}
+ }
+
+ ->Now in this step, we will print the answer subarray using the start and end variables.
+
+ ->Return the largest subarray sum that is:- msf.
  */
