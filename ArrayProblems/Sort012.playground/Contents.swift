c1122a1e3744151a8c70849ee92a6b04062f6a52
Pritam Dash <pritamdash2015@outlook.com>
2022-06-27 18:29:39 +0530

    Different approaches mentioned.

--- ArrayProblems/Sort012.playground/Contents.swift
+++ ArrayProblems/Sort012.playground/Contents.swift
@@ -15,4 +15,56 @@ import Cocoa
 
  Input: nums = [0]
  Input: nums = [0]
+ 
+ Solution 2: Keeping count of values
+
+ Intuition: Since in this case there are only 3 distinct values in the array so it’s easy to maintain the count of all, Like the count of 0, 1, and 2. This can be followed by overwriting the array based on the frequency(count) of the values.
+
+ Approach:
+
+     Take 3 variables to maintain the count of 0, 1 and 2.
+     Travel the array once and increment the corresponding counting variables
+
+ ( let’s consider count_0 = a, count_1 = b, count_2 = c )
+
+     In 2nd traversal of array, we will now over write the first ‘a’ indices / positions in array with ’0’, the next ‘b’ with ‘1’ and the remaining ‘c’ with ‘2’.
+
+ Time Complexity: O(N) + O(N)
+
+ Space Complexity: O(1)
+
+ Solution 3: 3-Pointer approach
+
+ This problem is a variation of the popular Dutch National flag algorithm
+
+ Intuition: In this approach, we will be using 3 pointers named low, mid, and high. We will be using these 3 pointers to move around the values. The primary goal here is to move 0s to the left and 2s to the right of the array and at the same time all the 1s shall be in the middle region of the array and hence the array will be sorted.
+
+ Approach:
+
+     Initialize the 3 pointers such that low and mid will point to 0th index and high pointer will point to last index
+
+ int low = 0
+
+ int mid = 0
+
+ int high = n-1
+
+     Now there will 3 different operations / steps based on the value of arr[mid] and will be repeated until mid <= high.
+
+
+     arr[mid] == 0:
+     swap(arr[low], arr[mid])
+     low++, mid++
+
+
+     arr[mid] == 1:
+     mid++
+
+
+     arr[mid] == 2:
+     swap(arr[mid], arr[high])
+     high–;
+
+ The array formed after these steps will be a sorted array.
  */
+
