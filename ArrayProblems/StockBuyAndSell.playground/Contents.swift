a095ea27a4f2f7cc58842da4b5b20fe163c1675e
Pritam Dash <pritamdash2015@outlook.com>
2022-06-27 21:15:22 +0530

    2nd approach added.

--- ArrayProblems/StockBuyAndSell.playground/Contents.swift
+++ ArrayProblems/StockBuyAndSell.playground/Contents.swift
@@ -59,3 +59,22 @@ print(maxProfit([7,1,5,3,6,4]))
  Space Complexity: O(1)
  */
 
+/*
+ Soltuion 2:Optimal solution
+
+ Intuition: We will linearly travel the array. We can maintain a minimum from the starting of the array and compare it with every element of the array, if it is greater than the minimum then take the difference and maintain it in max, otherwise update the minimum.
+
+ Approach:
+
+     Create a variable maxPro and mark it as 0.
+     Create a variable minPrice and mark it as max_value.
+     Run a for loop from 0 to n.
+     Update the minPrice if it is greater than the current element of the array
+     Take the difference of the minPrice with the current element of the array and compare and maintain it in maxPro.
+     Return the maxPro.
+ */
+
+
+
+
+
