ab58248c91efbe02189e6c507b5a7f7149acb5d5
Pritam Dash <pritamdash2015@outlook.com>
2022-06-28 23:57:51 +0530

    Approach 2 added.

--- ArrayProblems/RotateMatrix90.playground/Contents.swift
+++ ArrayProblems/RotateMatrix90.playground/Contents.swift
@@ -37,4 +37,16 @@ func rotateMatricBrute(matrix : [[Int]]) -> [[Int]]{
 
 print(rotateMatricBrute(matrix: [[1,2,3],[4,5,6],[7,8,9]]))
 
+/*
+ Solution 2: Optimized approach
+
+ Intuition: By observation, we see that the first column of the original matrix is the reverse of the first row of the rotated matrix, so that’s why we transpose the matrix and then reverse each row, and since we are making changes in the matrix itself space complexity gets reduced to O(1).
+
+ Approach:
+
+ Step1: Transpose of the matrix. (transposing means changing columns to rows and rows to columns)
+
+ Step2: Reverse each row of the matrix.
+ */
+
 
