7716f91c2cbc7027860104e4292eb3a5279e4d35
Pritam Dash <pritamdash2015@outlook.com>
2022-06-30 21:16:37 +0530

    Approach 1 with TC added.

--- ArrayProblems/Merge-Overlapping-Sub-intervals.playground/Contents.swift
+++ ArrayProblems/Merge-Overlapping-Sub-intervals.playground/Contents.swift
@@ -22,3 +22,14 @@ import Cocoa
 
  Explanation: Since intervals [1,3] and [2,6] are overlapping we can merge them to form [1,6] intervals.
  */
+
+/*
+ Solution 1: Brute force
+
+ Approach: First check whether the array is sorted or not.If not sort the array. Now linearly iterate over the array and then check for all of its next intervals whether they are overlapping with the interval at the current index. Take a new data structure and insert the overlapped interval. If while iterating if the interval lies in the interval present in the data structure simply continue and move to the next interval.
+ 
+ Time Complexity: O(NlogN)+O(N*N). O(NlogN) for sorting the array, and O(N*N) because we are checking to the right for each index which is a nested loop.
+
+ Space Complexity: O(N), as we are using a separate data structure.
+ */
+
