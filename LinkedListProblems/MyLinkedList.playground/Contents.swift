84e41359e4dd307746411203e546017603da1742
Pritam Dash <pritamdash2015@outlook.com>
2022-09-27 18:46:11 +0530

    2 more linkedList functions added.

--- LinkedListProblems/MyLinkedList.playground/Contents.swift
+++ LinkedListProblems/MyLinkedList.playground/Contents.swift
@@ -1,6 +1,50 @@
 import Foundation
 
 //Creating my own linkedlist for practice
+class Node {
+    var data : Int
+    var next : Node?
+    
+    init(_ data : Int, _ next : Node? = nil) {
+        //incase of tail , node can be nil hence optional
+        self.data = data
+        self.next = next
+    }
+}
+
+class LinkedList {
+    private var head : Node?
+    
+    func printLinkedList() {
+        guard head != nil else { return }
+        
+        var result : [Int] = []
+        
+        var node = head
+        
+        result.append(node!.data)
+        
+        while node != nil {
+            result.append(node!.data)
+            node = node!.next
+        }
+        print(result)
+    }
+    
+    func addFront(_ data : Int ) {
+        let newNode = Node(data)
+        newNode.next = head
+        head = newNode
+    }
+    
+    func getFirst() -> Int? {
+        if head == nil {
+            return nil
+        }
+        return head!.data
+    }
+}
+
 
 
 
