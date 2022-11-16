0ce190827fd15a74a1569137b40ff12671ffa624
Pritam Dash <pritamdash2015@outlook.com>
2022-11-16 22:06:00 +0530

    Pattern 6 done

---
+++ Striver_AtoZ/1_Basics/Logic Building/Pattern 7.playground/Contents.swift
@@ -0,0 +1,53 @@
+/*
+ Example 1:
+
+ Input: 5
+
+ Output:
+
+     *
+    ***
+   *****
+  *******
+ *********
+
+ Your Task:
+
+ You don't need to input anything. Complete the function printTriangle() which takes  an integer n  as the input parameter and print the pattern.
+
+ Constraints:
+
+     1<= N <= 20
+
+ */
+import Foundation
+
+func printTriangle(_ n : Int){
+    for i in 1...n{
+        
+        for _ in stride(from: n-1, to: i-1, by: -1){
+            print(" ", terminator: "")
+        }
+        
+        for _ in 1...(2*i-1){
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
+
+printTriangle(5)
+
+/*
+
+ Output :
+ 
+    *
+   ***
+  *****
+ *******
+*********
+
+
+ */
+
