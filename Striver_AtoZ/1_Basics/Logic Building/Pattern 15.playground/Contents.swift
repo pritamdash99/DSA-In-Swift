cf1463a4c315b728c6c26664cf038f5fdaa8b6f8
Pritam Dash <pritamdash2015@outlook.com>
2022-11-18 21:48:52 +0530

    Pattern 15 solved

---
+++ Striver_AtoZ/1_Basics/Logic Building/Pattern 15.playground/Contents.swift
@@ -0,0 +1,49 @@
+/*
+ Example 1:
+
+ Input: 5
+
+ Output:
+ 
+ ABCDE
+ ABCD
+ ABC
+ AB
+ A
+
+ Your Task:
+
+ You don't need to input anything. Complete the function printTriangle() which takes  an integer n  as the input parameter and print the pattern.
+
+ Constraints:
+
+     1<= N <= 20
+
+ */
+import Foundation
+
+func printTriangle(_ n : Int){
+    let sentance = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let arr = Array(sentance)
+    for i in stride(from: n, to: 0, by: -1){
+        for j in 0..<i{
+            print(arr[j],"",terminator: "")
+        }
+        print("")
+    }
+}
+
+printTriangle(5)
+
+/*
+ Output :
+ 
+ A
+ A B
+ A B C
+ A B C D
+ A B C D E
+
+ */
+
+
