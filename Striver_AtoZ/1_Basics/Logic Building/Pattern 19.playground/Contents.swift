f6d9461181fd7b925189629b0f55b7a41ca8c6ea
Pritam Dash <pritamdash2015@outlook.com>
2022-11-19 23:45:20 +0530

    Pattern 19 created

---
+++ Striver_AtoZ/1_Basics/Logic Building/Pattern 19.playground/Contents.swift
@@ -0,0 +1,53 @@
+/*
+ Example 1:
+
+ Input: 5
+
+ Output:
+ 
+ **********
+ ****  ****
+ ***    ***
+ **      **
+ *        *
+ *        *
+ **      **
+ ***    ***
+ ****  ****
+ **********
+ 
+ Your Task:
+
+ You don't need to input anything. Complete the function printTriangle() which takes  an integer n  as the input parameter and print the pattern.
+
+ Constraints:
+
+     1<= N <= 20
+
+ */
+import Foundation
+
+func printTriangle(_ n : Int){
+    let sentance = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let arr = Array(sentance)
+    for i in 1...n{
+        
+        print("")
+    }
+}
+
+printTriangle(5)
+
+/*
+ Output :
+ 
+ E
+ E D
+ E D C
+ E D C B
+ E D C B A
+
+
+ */
+
+
