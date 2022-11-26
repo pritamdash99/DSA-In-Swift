423949c5cfaf1389b6ab6c443ddcd6ecac0e4a4b
Pritam Dash <pritamdash2015@outlook.com>
2022-11-26 11:55:11 +0530

    Solved

--- Striver_AtoZ/1_Basics/Basic Maths/Reverse a number.playground/Contents.swift
+++ Striver_AtoZ/1_Basics/Basic Maths/Reverse a number.playground/Contents.swift
@@ -16,7 +16,41 @@
  Explanation: The reverse of 234 is 432
  */
 import Foundation
+import Darwin
+/*
+ Approach :
+ To get the last digit of a number use %10 and store the remainder.
+ To reduce the number of digits use /10 and store the result
+ To add another digit at the end of a number use *10 to current number and +digit to the new number(after *10)
+ 
+ */
 
-func reverseNumber(_ n : Int) -> Int{
+func reverseNumber(_ x : Int) -> Int{
+    var negativeSign = 1
+    var currentNumber = x
+    var remainder = 0
+    var newNumber = 0
     
+    if currentNumber < 0 {
+        negativeSign = -1
+        currentNumber *= negativeSign
+    }
+    
+    while currentNumber != 0 {
+        remainder = currentNumber % 10
+        currentNumber /= 10
+        newNumber = newNumber * 10 + remainder
+    }
+    newNumber *= negativeSign
+    
+    guard newNumber < Int32.max && newNumber > Int32.min else {
+        return 0
+    }
+    return newNumber
 }
+
+print(reverseNumber(1234)) // 4321
+print(reverseNumber(74836782728281918)) // 0
+/*
+ TC : O(n) , SC : O(1)
+ */
