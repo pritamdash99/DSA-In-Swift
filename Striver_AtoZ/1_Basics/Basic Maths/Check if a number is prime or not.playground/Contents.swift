6cce9a066d9e44e4a1a63629930ff754ef5169e6
Pritam Dash <pritamdash2015@outlook.com>
2022-11-30 20:30:15 +0530

    Check Prime Added

---
+++ Striver_AtoZ/1_Basics/Basic Maths/Check if a number is prime or not.playground/Contents.swift
@@ -0,0 +1,19 @@
+/*
+ Check if a number is prime or not
+ Problem Statement: Given a number, check whether it is prime or not. A prime number is a natural number that is only divisible by 1 and by itself.
+
+ Examples 1 2 3 5 7 11 13 17 19 …
+
+ Examples:
+
+ Example 1:
+ Input: N = 3
+ Output: Prime
+ Explanation: 3 is a prime number
+
+ Example 2:
+ Input: N = 26
+ Output: Non-Prime
+ Explanation: 26 is not prime
+ */
+
