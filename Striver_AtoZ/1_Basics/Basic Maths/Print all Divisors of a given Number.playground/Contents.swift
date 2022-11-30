61ab5ecf80c0de092d64e48f037a6d1609f350e3
Pritam Dash <pritamdash2015@outlook.com>
2022-11-30 13:45:25 +0530

    First Approach added

--- Striver_AtoZ/1_Basics/Basic Maths/Print all Divisors of a given Number.playground/Contents.swift
+++ Striver_AtoZ/1_Basics/Basic Maths/Print all Divisors of a given Number.playground/Contents.swift
@@ -17,3 +17,34 @@
  */
 import Foundation
 
+/*
+ Intuition:
+ As we know the divisors of a number will definitely be lesser or equal to the number, all the numbers between 1 and the number, are the possible candidates for the divisors.
+
+ Approach:
+     This is the basic approach. As we know the possible candidates, we iterate upon all the candidates and check whether they divide the actual number.
+     If it divides, then it is one of the divisors. Therfore, we print it.
+     If it does not divide, then it is not a divisor. We do this for all the candidates.
+ 
+ */
+func printDivisors1(_ n : Int){
+    print("Start for n =", n)
+    for i in 1...n{
+        if n % i == 0 {
+            print(i,"", terminator: "")
+        }
+    }
+    print("\nDone")
+}
+
+printDivisors1(36)
+
+/*
+ O/p :
+ Start for n = 36
+ 1 2 3 4 6 9 12 18 36
+ Done
+
+ Time Complexity: O(n), because the loop has to run from 1 to n always.
+ Space Complexity: O(1), we are not using any extra space.
+ */
