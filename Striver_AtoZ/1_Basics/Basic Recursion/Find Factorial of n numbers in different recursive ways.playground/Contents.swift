e9ca115d813204098b5b68fe6a8cdae2edb9b34a
Pritam Dash <pritamdash2015@outlook.com>
2022-12-03 20:55:57 +0530

    Print factorial of n added

---
+++ Striver_AtoZ/1_Basics/Basic Recursion/Find Factorial of n numbers in different recursive ways.playground/Contents.swift
@@ -0,0 +1,23 @@
+/*
+ Find the factorial of n numbers in parametrised as well as functional recursion.
+ */
+import Foundation
+
+//parametrised way
+//func findFactorial1(_ n :Int, _ product : Int){
+//    if n < 1 {
+//        print(product)
+//    }
+//    findFactorial1(n-1,product * n)
+//}
+//findFactorial1(5, 1)
+
+//functional way
+
+func findFactorial2(_ n :Int) -> Int{
+    if n == 1 {
+        return 1
+    }
+    return n * findFactorial2(n-1)
+}
+print(findFactorial2(5))
