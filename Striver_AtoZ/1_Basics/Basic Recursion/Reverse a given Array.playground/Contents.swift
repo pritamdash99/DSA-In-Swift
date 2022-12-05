a792c2e11021f1c4470349ee71e5d97029c8a6f1
Pritam Dash <pritamdash2015@outlook.com>
2022-12-05 20:20:02 +0530

    Reverse an array using recursion done

---
+++ Striver_AtoZ/1_Basics/Basic Recursion/Reverse a given Array.playground/Contents.swift
@@ -0,0 +1,47 @@
+/*
+ Reverse a given Array
+
+ Problem Statement: You are given an array. The task is to reverse the array and print it.
+
+ Examples:
+
+ Example 1:
+ Input: N = 5, arr[] = {5,4,3,2,1}
+ Output: {1,2,3,4,5}
+ Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
+
+ Example 2:
+ Input: N=6 arr[] = {10,20,30,40}
+ Output: {40,30,20,10}
+ Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
+ 
+ */
+import Foundation
+/*
+ Approach: The recursive method has an approach almost similar to the iterative one. The approach has been broken down into some steps for simplicity.
+
+     Create a function that takes an array ,start index and end index of the array as parameters.
+     Swap the elements present  at start and end index,
+     The portion of array left to be reversed is arr[start+1,end-1]. Make a recursive call to reverse the rest of array. While calling recursion pass start +1  and end – 1 as parameters for the shrinked array. Repeat step 2.
+     Continue recursion as long as ‘start < end’ condition is satisfied. This is the base case for our recursion.
+ */
+func reverseArrayUsingRecursion(_ arr : [Int]){
+    var arr = arr
+    reverseArray(0,arr.count - 1, &arr)
+    print(arr)
+}
+
+func reverseArray(_ l : Int, _ r : Int, _ arr : inout [Int]){
+    //base condition
+    if l < r {
+        let temp = arr[l]
+        arr[l] = arr[r]
+        arr[r] = temp
+        reverseArray(l+1,r-1, &arr)
+    }
+}
+
+var arr = [5,4,3,2,1]
+reverseArrayUsingRecursion(arr)
+// [1, 2, 3, 4, 5]
+
