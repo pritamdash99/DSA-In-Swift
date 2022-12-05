3a9980b06916f69f1c107d079ca963f27fde605d
Pritam Dash <pritamdash2015@outlook.com>
2022-12-05 20:28:52 +0530

    Reverse a given array using recursion done using 1 pointer

--- Striver_AtoZ/1_Basics/Basic Recursion/Reverse a given Array.playground/Contents.swift
+++ Striver_AtoZ/1_Basics/Basic Recursion/Reverse a given Array.playground/Contents.swift
@@ -25,23 +25,58 @@ import Foundation
      The portion of array left to be reversed is arr[start+1,end-1]. Make a recursive call to reverse the rest of array. While calling recursion pass start +1  and end – 1 as parameters for the shrinked array. Repeat step 2.
      Continue recursion as long as ‘start < end’ condition is satisfied. This is the base case for our recursion.
  */
-func reverseArrayUsingRecursion(_ arr : [Int]){
-    var arr = arr
-    reverseArray(0,arr.count - 1, &arr)
-    print(arr)
-}
+class TwoPointer{
+    func reverseArrayUsingRecursion(_ arr : [Int]){
+        var arr = arr
+        reverseArray(0,arr.count - 1, &arr)
+        print(arr)
+    }
 
-func reverseArray(_ l : Int, _ r : Int, _ arr : inout [Int]){
-    //base condition
-    if l < r {
-        let temp = arr[l]
-        arr[l] = arr[r]
-        arr[r] = temp
-        reverseArray(l+1,r-1, &arr)
+    func reverseArray(_ l : Int, _ r : Int, _ arr : inout [Int]){
+        //base condition
+        if l < r {
+            let temp = arr[l]
+            arr[l] = arr[r]
+            arr[r] = temp
+            reverseArray(l+1,r-1, &arr)
+        }
     }
 }
 
 var arr = [5,4,3,2,1]
-reverseArrayUsingRecursion(arr)
+let x = TwoPointer()
+x.reverseArrayUsingRecursion(arr)
 // [1, 2, 3, 4, 5]
 
+
+/*
+ Using only 1 pointer i.
+ Here use i and array.count - i - 1
+ base condition would be i > array.count / 2
+ */
+class OnePointer{
+    func reverseArrayUsingRecursion(_ arr : [Int]){
+        var arr = arr
+        reverseArray(0,&arr)
+        print(arr)
+    }
+
+    func reverseArray(_ i : Int, _ arr : inout [Int]){
+        let n = arr.count
+        //base condition
+        if i > n / 2{
+            return
+        }
+        
+        //swap
+        let temp = arr[i]
+        arr[i] = arr[n-i-1]
+        arr[n-i-1] = temp
+        
+        reverseArray(i+1, &arr)
+    }
+}
+
+let y = OnePointer()
+y.reverseArrayUsingRecursion(arr)
+// [1, 2, 3, 4, 5]
